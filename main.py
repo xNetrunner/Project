f73b23ecdffa14415b2a227c561a127777132549
xNetrunner <inikita2308@gmail.com>
2024-11-07 23:52:14 +0300

    new

---
+++ main.py
@@ -0,0 +1,16 @@
+
+name = "Ivan" # str
+age = 25 # int
+avg = 3.25 # float
+l123 = True # bool
+print(type(name)) # <class 'str'>
+print(type(age)) # <class 'int'>
+print(type(avg)) # <class 'float'>
+print(type(l123)) # <class 'bool'>
+# type('переменная')
+
+# input('комментарий')
+# int ('строка')
+name2 = int(input('Введите что-нибудь: ')) # 25
+print("То, что вы ввели",name2)
+print("Тип данных:",type(name2))
